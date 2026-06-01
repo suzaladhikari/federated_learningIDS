c60c353c381bf693ee4b95cb03c26ecda6033e1b
Peter <adhikarisuzal944@gmail.com>
2026-06-01 17:39:25 -0400

    all about nn.ModuleList

--- FederatedLearningModel/fedmodel.py
+++ FederatedLearningModel/fedmodel.py
@@ -5,4 +5,5 @@ import math
 class DNN(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super().__init__()
-        self.fc_layers = 
+        self.fc_layers = nn.ModuleList() ## We are using nn.ModuleList instead of nn.Sequential becuase we need to append the layers later on  
+
