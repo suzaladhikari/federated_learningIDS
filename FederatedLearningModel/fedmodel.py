198a281f2acbf0ca633388134c86219fd1fca917
Peter <adhikarisuzal944@gmail.com>
2026-06-01 18:00:38 -0400

    dense layer calculated

--- FederatedLearningModel/fedmodel.py
+++ FederatedLearningModel/fedmodel.py
@@ -8,6 +8,11 @@ class DNN(nn.Module):
         self.fc_layers = nn.ModuleList() ## We are using nn.ModuleList instead of nn.Sequential becuase we need dynamic control over the layer construction and forward execution.
         layer_sizes = [input_size] + hidden_size 
         for i in range(len(hidden_size)):
-            self.fc_layers.append(nn.Linear(layer_sizes[i], layer_sizes[i+1]))
+            self.fc_layers.append(nn.Linear(layer_sizes[i], layer_sizes[i+1])) ## Lets assume the input is 1568 and hidden layer is [512,64,32,9]
+            ### In such way the layers become (1568,512), (512,64), (64,32), (32,9)
+            ### Between each layers we adapt the activation function oif leakyRely with negative slope of 0.001
+            self.fc_layers.append(nn.LeakyReLU(negative_slope=0.001))
+        self.fc_layers.append(nn.Linear(hidden_size[-1], num_classes)) ## This is used as the dense layer where we just convert the given last hidden layer to the number of classes 
+        
 
 
