04365ef6fed3eca4ccc0e5d07a7123a09baa9275
Peter <adhikarisuzal944@gmail.com>
2026-06-01 17:52:49 -0400

    fc layers in dnn

--- FederatedLearningModel/fedmodel.py
+++ FederatedLearningModel/fedmodel.py
@@ -5,5 +5,9 @@ import math
 class DNN(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super().__init__()
-        self.fc_layers = nn.ModuleList() ## We are using nn.ModuleList instead of nn.Sequential becuase we need to append the layers later on  
+        self.fc_layers = nn.ModuleList() ## We are using nn.ModuleList instead of nn.Sequential becuase we need dynamic control over the layer construction and forward execution.
+        layer_sizes = [input_size] + hidden_size 
+        for i in range(len(hidden_size)):
+            self.fc_layers.append(nn.Linear(layer_sizes[i], layer_sizes[i+1]))
+
 
