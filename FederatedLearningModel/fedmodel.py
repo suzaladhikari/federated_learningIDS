cd325a8b31dd3476ee5ef0fbbddbd84b40a051b2
Peter <adhikarisuzal944@gmail.com>
2026-06-04 08:46:11 -0400

    changed the name of the model from dnn to mlp

--- FederatedLearningModel/fedmodel.py
+++ FederatedLearningModel/fedmodel.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 import math 
 
-class DNN(nn.Module):
+class MLP(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super().__init__()
         self.fc_layers = nn.ModuleList() ## We are using nn.ModuleList instead of nn.Sequential becuase we need dynamic control over the layer construction and forward execution.
