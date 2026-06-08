058b1add5228c6f6c40ede3b4bcf6034ff2b3762
Peter <adhikarisuzal944@gmail.com>
2026-06-08 17:21:39 -0400

    imported all the necessary libararies

--- utlis.py
+++ utlis.py
@@ -3,3 +3,8 @@ import pickle
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import functional as TF
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
