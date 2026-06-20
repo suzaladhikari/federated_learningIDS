353cb0cd4e1384a100faa13151c00b1410111a11
Peter <adhikarisuzal944@gmail.com>
2026-06-20 13:58:32 -0400

    total samples added

--- federatedlearning.py
+++ federatedlearning.py
@@ -155,3 +155,8 @@ def fednova_update_from_local(global_model, client_loader, test_loader, num_loca
 
 
 
+### Purposed Solution: Hierarchical Federated Nova Weight Calculation
+def hierar_fednova_weight_averageing(global_model, weight_list, num_samples, tau_k, device, alpha = 0.5):
+    global_weights = global_model.state_dict()
+    keys = weight_list[0].keys()
+    total_samples = sum(num_samples)
